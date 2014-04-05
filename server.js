39ec95ff58982cd5c1f7fd1ebb667f0bd26a0e82
Jared Likes <likesjx@gmail.com>
2014-04-05 16:58:04 -0400

    fullstack generator reran

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 'use strict';
 
-var express = require('express'),
-    path = require('path'),
-    fs = require('fs'),
-    mongoose = require('mongoose');
+var express = require('express');
 
 /**
  * Main application file
@@ -14,23 +11,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 // Application Config
 var config = require('./lib/config/config');
-
-// Connect to database
-var db = mongoose.connect(config.mongo.uri, config.mongo.options);
-
-// Bootstrap models
-var modelsPath = path.join(__dirname, 'lib/models');
-fs.readdirSync(modelsPath).forEach(function (file) {
-  if (/(.*)\.(js$|coffee$)/.test(file)) {
-    require(modelsPath + '/' + file);
-  }
-});
-
-// Populate empty DB with sample data
-require('./lib/config/dummydata');
-  
-// Passport Configuration
-var passport = require('./lib/config/passport');
 
 var app = express();
 
