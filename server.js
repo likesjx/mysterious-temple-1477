283c14a8528eb413e47b69e2e2b67f357557caa2
Jared Likes <likesjx@gmail.com>
2014-04-06 00:42:17 -0400

    getting it work on heroku

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 'use strict';
 
-var express = require('express');
+var express = require('express'),
+    path = require('path'),
+    fs = require('fs'),
+    mongoose = require('mongoose');
 
 /**
  * Main application file
@@ -8,9 +11,27 @@ var express = require('express');
 
 // Set default node environment to development
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.PWD = process.cwd();
 
 // Application Config
 var config = require('./lib/config/config');
+
+// Connect to database
+var db = mongoose.connect(config.mongo.uri, config.mongo.options);
+
+// Bootstrap models
+var modelsPath = path.join(__dirname, 'lib/models');
+fs.readdirSync(modelsPath).forEach(function (file) {
+  if (/(.*)\.(js$|coffee$)/.test(file)) {
+    require(modelsPath + '/' + file);
+  }
+});
+
+// Populate empty DB with sample data
+require('./lib/config/dummydata');
+  
+// Passport Configuration
+var passport = require('./lib/config/passport');
 
 var app = express();
 
@@ -20,6 +41,7 @@ require('./lib/config/express')(app);
 // Routing
 require('./lib/routes')(app);
 
+console.log('Express server attempting to listen on port %d in %s mode', config.port, app.get('env'));
 // Start server
 app.listen(config.port, function () {
   console.log('Express server listening on port %d in %s mode', config.port, app.get('env'));
